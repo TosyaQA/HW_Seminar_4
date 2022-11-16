d3ff326f4e4b70bf85cabe5ec5fa786596ee027f
Uliana Udalova <uljanaud@gmail.com>
2022-11-16 22:41:05 +0300

    Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+// Для решения задания использование цикла for является обязательным условием
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+if (numberN >= 10) {
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+} else {
+    Console.WriteLine($"Введите двухзначное положительное число");
+}
